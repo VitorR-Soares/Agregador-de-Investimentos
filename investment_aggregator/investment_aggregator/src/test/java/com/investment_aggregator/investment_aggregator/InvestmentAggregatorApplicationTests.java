74690176f88bf5f20e5e6036afbdd11a9092e199
VitorR-Soares <vitorratollasoares@gmail.com>
2025-01-22 11:54:33 -0300

    First Commit

---
+++ investment_aggregator/investment_aggregator/src/test/java/com/investment_aggregator/investment_aggregator/InvestmentAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investment_aggregator.investment_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
