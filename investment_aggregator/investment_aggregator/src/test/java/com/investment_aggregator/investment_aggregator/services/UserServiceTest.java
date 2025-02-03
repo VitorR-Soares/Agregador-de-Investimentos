8baf6569976c667738e6645dc575e5d7aef76cf4
VitorR-Soares <vitorratollasoares@gmail.com>
2025-02-03 10:28:13 -0300

    UserServiceTest created

---
+++ investment_aggregator/investment_aggregator/src/test/java/com/investment_aggregator/investment_aggregator/services/UserServiceTest.java
@@ -0,0 +1,48 @@
+package com.investment_aggregator.investment_aggregator.services;
+
+import com.investment_aggregator.investment_aggregator.entities.User;
+import com.investment_aggregator.investment_aggregator.repositories.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    UserRepository repository;
+
+    @InjectMocks
+    UserService service;
+
+    @Captor
+    private ArgumentCaptor<User> userArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<UUID> uuidArgumentCaptor;
+
+    @Nested
+    class createUser {
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
